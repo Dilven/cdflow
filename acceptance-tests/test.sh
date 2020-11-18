0612c164246982e37ffa49d6c054046a332e5a1f
Gary Richards <gary.richards@acuris.com>
2020-11-18 12:30:36 +0000

    For docker pull limits support auth in the acceptance test
    Support auth from the acceptance test in the same ways as it's supported
    by cdflow itself for consistency

--- acceptance-tests/test.sh
+++ acceptance-tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euxo pipefail
 
 IMAGE_ID="$(basename $(pwd))-acceptance-test"
 AC_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -13,12 +13,38 @@ docker image build -t "${IMAGE_ID}" \
 root=$(git rev-parse --show-toplevel)
 
 echo "Running the acceptance tests within the ${IMAGE_ID} container"
-docker container run \
-    --name "${IMAGE_ID}" \
-    --rm \
-    -e CDFLOW_IMAGE_ID=${CDFLOW_IMAGE_ID:-mergermarket/cdflow-commands:latest} \
-    -w $AC_FOLDER \
-    -v ${root}:${root} \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -i $(tty >/dev/null && echo -t) \
-    "${IMAGE_ID}" cdflow --help
+
+if [ -n "${DOCKERHUB_USERNAME:-}" ] && [ -n "${DOCKERHUB_USERNAME:-}" ]; then
+    docker container run \
+        --name "${IMAGE_ID}" \
+        --rm \
+        -e CDFLOW_IMAGE_ID=${CDFLOW_IMAGE_ID:-mergermarket/cdflow-commands:latest} \
+        -e DOCKERHUB_USERNAME=${DOCKERHUB_USERNAME} \
+        -e DOCKERHUB_PASSWORD=${DOCKERHUB_PASSWORD} \
+        -w $AC_FOLDER \
+        -v ${root}:${root} \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -i $(tty >/dev/null && echo -t) \
+        "${IMAGE_ID}" cdflow --help
+elif [ -e ~/.docker/config.json ]; then
+    docker container run \
+        --name "${IMAGE_ID}" \
+        --rm \
+        -e CDFLOW_IMAGE_ID=${CDFLOW_IMAGE_ID:-mergermarket/cdflow-commands:latest} \
+        -w $AC_FOLDER \
+        -v ${root}:${root} \
+        -v ~/.docker/config.json:/root/.docker/config.json \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -i $(tty >/dev/null && echo -t) \
+        "${IMAGE_ID}" cdflow --help
+else
+    docker container run \
+        --name "${IMAGE_ID}" \
+        --rm \
+        -e CDFLOW_IMAGE_ID=${CDFLOW_IMAGE_ID:-mergermarket/cdflow-commands:latest} \
+        -w $AC_FOLDER \
+        -v ${root}:${root} \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -i $(tty >/dev/null && echo -t) \
+        "${IMAGE_ID}" cdflow --help
+fi
